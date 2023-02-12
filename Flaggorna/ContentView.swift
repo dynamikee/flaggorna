13cd12d1df26062bb018dda3530e4fc71d498873
Micke Mattsson <mikael.mattsson@nentgroup.com>
2023-02-12 17:14:43 +0100

    Scene structure + json and flag images added. Great start :)

--- Flaggorna/ContentView.swift
+++ Flaggorna/ContentView.swift
@@ -8,19 +8,105 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentScene = "Start"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        switch currentScene {
+        case "Start":
+            StartGameView(currentScene: $currentScene)
+        case "Main":
+            MainGameView(currentScene: $currentScene)
+        case "Right":
+            RightAnswerView(currentScene: $currentScene)
+        case "Wrong":
+            WrongAnswerView(currentScene: $currentScene)
+        case "GameOver":
+            GameOverView(currentScene: $currentScene)
+        default:
+            StartGameView(currentScene: $currentScene)
+        }
+    }
+}
+
+struct StartGameView: View {
+    
+    @Binding var currentScene: String
+    
+    var body: some View {
+        Button(action: {
+            currentScene = "Main"
+        }){
+            Text("Start game")
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct MainGameView: View {
+    
+    @Binding var currentScene: String
+    
+    var body: some View {
+        Button(action: {
+            currentScene = "Right"
+        }){
+            Text("Right")
+        }
+        .padding()
+        Button(action: {
+            currentScene = "Wrong"
+        }){
+            Text("Wrong")
+        }
+        .padding()
     }
 }
+
+struct RightAnswerView: View {
+    
+    @Binding var currentScene: String
+    
+    var body: some View {
+        Button(action: {
+            currentScene = "Main"
+        }){
+            Text("Continue..")
+        }
+        .padding()
+    }
+    
+}
+
+struct WrongAnswerView: View {
+    
+    @Binding var currentScene: String
+    
+    var body: some View {
+        Button(action: {
+            currentScene = "GameOver"
+        }){
+            Text("Continue..")
+        }
+        .padding()
+    }
+    
+}
+
+
+
+struct GameOverView: View {
+    
+    @Binding var currentScene: String
+    
+    var body: some View {
+        Button(action: {
+            currentScene = "Start"
+        }){
+            Text("GAME OVER !")
+        }
+        .padding()
+    }
+}
+
+
