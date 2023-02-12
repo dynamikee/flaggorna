c3764cd686bd2705ffa87fb610e1a7a7b9d995d5
Micke Mattsson <mikael.mattsson@nentgroup.com>
2023-02-12 22:28:08 +0100

    Right wrong screens with timer and animation

--- Flaggorna/ContentView.swift
+++ Flaggorna/ContentView.swift
@@ -1,5 +1,6 @@
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     @State var currentScene = "Start"
@@ -79,21 +80,21 @@ struct MainGameView: View {
 
     var body: some View {
         let randomCountry = countries.randomElement()!
-                    let currentCountry = randomCountry.name
-                    let countryAlternatives = countries.filter { $0.name != currentCountry }
-                    var randomCountryNames = countryAlternatives.map { $0.name }.shuffled().prefix(3)
-                    randomCountryNames.append(currentCountry)
-                    randomCountryNames.shuffle()
+        let currentCountry = randomCountry.name
+        let countryAlternatives = countries.filter { $0.name != currentCountry }
+        var randomCountryNames = countryAlternatives.map { $0.name }.shuffled().prefix(3)
+        randomCountryNames.append(currentCountry)
+        randomCountryNames.shuffle()
 
         return VStack {
             Image(randomCountry.flag)
             ForEach(randomCountryNames, id: \.self) { countryName in
                 Button(action: {
                     if strcmp(currentCountry, countryName) == 0 {
-                                            self.currentScene = "Right"
-                                        } else {
-                                            self.currentScene = "Wrong"
-                                        }
+                        self.currentScene = "Right"
+                    } else {
+                        self.currentScene = "Wrong"
+                    }
                 }) {
                     Text(countryName)
                 }
@@ -102,34 +103,62 @@ struct MainGameView: View {
     }
 }
 
-
-
 struct RightAnswerView: View {
-    
     @Binding var currentScene: String
-    
+
     var body: some View {
-        Button(action: {
-            currentScene = "Main"
-        }){
-            Text("Continue..")
+        VStack {
+            Spacer()
+            Text("YOU ARE RIGHT!")
+            ZStack {
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 12, height: 12)
+                    .modifier(ParticlesModifier())
+                    .offset(x: -100, y : -50)
+                        
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 12, height: 12)
+                    .modifier(ParticlesModifier())
+                    .offset(x: 60, y : 70)
+            }
+            Spacer()
+            
         }
-        .padding()
+        .onAppear {
+            withAnimation {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    self.currentScene = "Main"
+                }
+            }
+        }
     }
-    
 }
+
+
+
+
 
 struct WrongAnswerView: View {
     
     @Binding var currentScene: String
     
     var body: some View {
-        Button(action: {
-            currentScene = "GameOver"
-        }){
-            Text("Continue..")
+        VStack {
+            Spacer()
+            Text("YOU ARE WRONG!")
+
+            Spacer()
+            
         }
-        .padding()
+        .onAppear {
+            withAnimation {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    self.currentScene = "Main"
+                }
+            }
+        }
     }
     
 }
@@ -147,6 +176,47 @@ struct GameOverView: View {
             Text("GAME OVER !")
         }
         .padding()
+    }
+}
+
+struct FireworkParticlesGeometryEffect : GeometryEffect {
+    var time : Double
+    var speed = Double.random(in: 20 ... 200)
+    var direction = Double.random(in: -Double.pi ...  Double.pi)
+    
+    var animatableData: Double {
+        get { time }
+        set { time = newValue }
+    }
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        let xTranslation = speed * cos(direction) * time
+        let yTranslation = speed * sin(direction) * time
+        let affineTranslation =  CGAffineTransform(translationX: xTranslation, y: yTranslation)
+        return ProjectionTransform(affineTranslation)
+    }
+}
+
+struct ParticlesModifier: ViewModifier {
+    @State var time = 0.0
+    @State var scale = 0.1
+    let duration = 3.0
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            ForEach(0..<80, id: \.self) { index in
+                content
+                    .hueRotation(Angle(degrees: time * 80))
+                    .scaleEffect(scale)
+                    .modifier(FireworkParticlesGeometryEffect(time: time))
+                    .opacity(((duration-time) / duration))
+            }
+        }
+        .onAppear {
+            withAnimation (.easeOut(duration: duration)) {
+                self.time = duration
+                self.scale = 1.0
+            }
+        }
     }
 }
 
