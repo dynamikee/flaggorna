6cbb38b2c2214903257f35c686f27f987c92bee8
Micke Mattsson <mikael.mattsson@nentgroup.com>
2023-02-12 16:45:08 +0100

    Initial Commit

---
+++ Flaggorna/FlaggornaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaggornaApp.swift
+//  Flaggorna
+//
+//  Created by Mikael Mattsson on 2023-02-12.
+//
+
+import SwiftUI
+
+@main
+struct FlaggornaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
