fb76cdabc2d538faceeab48304ca2bcdda9925fd
Micke Mattsson <mikael.mattsson@nentgroup.com>
2023-06-16 19:10:28 +0200

    Added a state that checks for purchase - Thank you for purchasing premium!

--- Flaggorna/StoreView.swift
+++ Flaggorna/StoreView.swift
@@ -13,38 +13,43 @@ struct StoreView: View {
     @EnvironmentObject private var purchaseManager: PurchaseManager
     
     var body: some View {
+        
         VStack(spacing: 20) {
-            Text("Play with more than four friends")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
             
-            Text("You need a premium subscription")
-                .font(.body)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-            
-            ForEach(purchaseManager.products) { product in
-                Button {
-                    Task {
-                        do {
-                            try await purchaseManager.purchase(product)
-                        } catch {
-                            print(error)
+            if purchaseManager.hasUnlockedPremium {
+                Text("Thank you for purchasing premium!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+            } else {
+                
+                Text("Play with more than four friends")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                Text("You need a premium subscription")
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                ForEach(purchaseManager.products) { product in
+                    Button {
+                        Task {
+                            do {
+                                try await purchaseManager.purchase(product)
+                            } catch {
+                                print(error)
+                            }
                         }
+                    } label: {
+                        Text("\(product.displayPrice)")
                     }
-                } label: {
-                    Text("\(product.displayPrice)")
+                    .padding()
+                    .buttonStyle(OrdinaryButtonStyle())
                 }
-                .padding()
-                .buttonStyle(OrdinaryButtonStyle())
-            }
-        }.task {
-            do {
-                try await purchaseManager.loadProducts()
-            } catch {
-                print(error)
             }
         }
         .padding(24)
