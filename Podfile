b83923a7cadc0c1eaca1b0267075e48083b39643
Micke Mattsson <micke@dynamike.se>
2023-09-03 20:36:50 +0200

    Trying to add workspace and pod files

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.0'
+
+target 'Flaggorna' do
+  use_frameworks!
+
+  # Pods for Flaggorna
+  pod 'Starscream'
+end
---
+++ Pods/Target Support Files/Pods-Flaggorna/Pods-Flaggorna-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flaggorna : NSObject
+@end
+@implementation PodsDummy_Pods_Flaggorna
+@end
